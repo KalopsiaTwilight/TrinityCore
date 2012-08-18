55c689f3c039145b3fb514aa6e8a07d5ff5e1e4e
Kretol <kretol@conquestofthehorde.com>
2012-08-18 01:33:26 -0700

    Testing targeted .gm commands for Admins Also try to fix random DB error concerning unknown column 'scale'

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1135,7 +1135,6 @@ class Player : public Unit, public GridObject<Player>
         void SetGameMaster(bool on);
         bool isGMChat() const { return m_ExtraFlags & PLAYER_EXTRA_GM_CHAT; }
         void SetGMChat(bool on) { if (on) m_ExtraFlags |= PLAYER_EXTRA_GM_CHAT; else m_ExtraFlags &= ~PLAYER_EXTRA_GM_CHAT; }
-        bool isAdministrator() const { return GetSession()->GetSecurity() == SEC_ADMINISTRATOR; }        
         bool isTaxiCheater() const { return m_ExtraFlags & PLAYER_EXTRA_TAXICHEAT; }
         void SetTaxiCheater(bool on) { if (on) m_ExtraFlags |= PLAYER_EXTRA_TAXICHEAT; else m_ExtraFlags &= ~PLAYER_EXTRA_TAXICHEAT; }
         bool isGMVisible() const { return !(m_ExtraFlags & PLAYER_EXTRA_GM_INVISIBLE); }
