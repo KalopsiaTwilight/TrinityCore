4b4d839b1cfa3d49a2bd91f764f6dce81852f3fa
Kretol <kretol@conquestofthehorde.com>
2013-08-27 01:13:54 -0700

    Remove double function of GetObjectScale (previously custom)

--- src/server/game/Entities/Object/Object.h
+++ src/server/game/Entities/Object/Object.h
@@ -192,7 +192,6 @@ class Object
         uint32 GetEntry() const { return GetUInt32Value(OBJECT_FIELD_ENTRY); }
         void SetEntry(uint32 entry) { SetUInt32Value(OBJECT_FIELD_ENTRY, entry); }
 
-        float GetObjectScale() const { return GetFloatValue(OBJECT_FIELD_SCALE_X); }
         virtual void SetObjectScale(float scale) { SetFloatValue(OBJECT_FIELD_SCALE_X, scale); }
         float GetObjectScale() const { return GetFloatValue(OBJECT_FIELD_SCALE_X); }
 
