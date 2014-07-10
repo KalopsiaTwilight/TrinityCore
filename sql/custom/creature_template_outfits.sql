82a304276156b52a3a45363efc5dcf96c172df7a
Kretol <kretol@conquestofthehorde.com>
2014-07-09 19:20:45 -0700

    creature_template_outfits table structure

---
+++ sql/custom/creature_template_outfits.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS `creature_template_outfits` (
+  `entry` int(10) unsigned NOT NULL,
+  `race` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  `gender` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '0 for male, 1 for female',
+  `skin` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `face` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `hair` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `haircolor` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `facialhair` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `head` int(10) NOT NULL DEFAULT '0',
+  `shoulders` int(10) NOT NULL DEFAULT '0',
+  `body` int(10) NOT NULL DEFAULT '0',
+  `chest` int(10) NOT NULL DEFAULT '0',
+  `waist` int(10) NOT NULL DEFAULT '0',
+  `legs` int(10) NOT NULL DEFAULT '0',
+  `feet` int(10) NOT NULL DEFAULT '0',
+  `wrists` int(10) NOT NULL DEFAULT '0',
+  `hands` int(10) NOT NULL DEFAULT '0',
+  `back` int(10) NOT NULL DEFAULT '0',
+  `tabard` int(10) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Use positive values for item entries and negative to use item displayid for head, shoulders etc.';
+
+ALTER TABLE `creature_template_outfits`
+ ADD PRIMARY KEY (`entry`);
