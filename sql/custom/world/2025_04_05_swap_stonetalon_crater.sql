3eb04328ae8b03da63940144133a1f79363d10e6
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2025-04-05 16:58:54 +0200

    Add stone talon crater terrain swap

---
+++ sql/custom/world/2025_04_05_swap_stonetalon_crater.sql
@@ -0,0 +1,3 @@
+-- DELETE FROM `terrain_swap_defaults` WHERE `TerrainSwapMap` = 1019;
+INSERT INTO `terrain_swap_defaults` (`MapId`, `TerrainSwapMap`, `Comment`) VALUES
+(1, 731, 'Stonetalon Bomb');
