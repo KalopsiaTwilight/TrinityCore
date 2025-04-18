6a2402e38b58b083653dba721d435fb0395b6f39
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2025-04-18 18:50:26 +0200

    Fix some more wrong command syntaxes.

---
+++ sql/custom/world/2025_04_18_fix_npc_follow_command.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `command` (name, help) VALUES
+('npc follow', 'Syntax: .npc follow \nSelected creature start follow you until death/fight/etc.')
+;
---
+++ sql/updates/freedom/2025_04_18_fix_npc_follow_command_syntax.sql
@@ -0,0 +1,3 @@
+REPLACE INTO `commands` (command, syntax, description, gmlevel) VALUES
+('npc follow', 'Syntax: .npc follow','Selected creature start follow you until death/fight/etc.',1)
+;
