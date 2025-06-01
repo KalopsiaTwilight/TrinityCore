4bb071523f199b9d6e355adbb9bfbfb8cdb110bb
KalopsiaTwilight <KalopsiaTwilight@gmail.com>
2025-06-01 09:31:47 +0200

    Add recent custom items

---
+++ sql/updates/freedom/2025_06_01_add_customitems.sql
@@ -0,0 +1,9 @@
+REPLACE INTO `item_template_extra` (`entry_id`, `hidden`, `name`, `inventory_type`, `class`, `subclass`) 
+VALUES 
+(259439, 0, 'Bhalandros'' Copper Greatsword of the Ebon Blade', 17, 2, 8),
+(259440, 0, 'Bhalandros'' Buckle-less Sinister Aspirant''s Silk Cord', 6, 4, 1),
+(259441, 0, 'Bhalandros'' Buckle-less Golden Xorothian Waistplate', 6, 4, 1),
+(259442, 0, 'Bhalandros'' Golden Draenei Gun', 15, 2, 3),
+(259443, 0, 'Bhalandros'' Fixed Golden Halberd of Agthia', 17, 2, 6),
+(259444, 0, 'Bhalandros'' Fixed Upright Golden Halberd of Agthia', 17, 2, 6)
+;
